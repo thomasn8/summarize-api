6b1f24b998c837795157c59ddc706d03cd393377
Thomas Nanchen <thomasnanchen@hotmail.com>
2024-05-26 19:47:08 +0200

    add linter and prettier

--- _build/back/start.sh
+++ _build/back/start.sh
@@ -9,11 +9,16 @@ fi
 if [ "$BUILD_TYPE" = "Started" ];
 then
 	npm install
+	npm install class-validator class-transformer
+	npm install --save-dev eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin
+	npm install --save-dev prettier
+	npm install --save-dev eslint-config-prettier eslint-plugin-prettier
 fi
 
 if [ "$BUILD_TYPE" = "Prod" ]; 
 then 
 	npm install
+	npm install class-validator class-transformer
 	npm build
   npm run start:prod
 else 
