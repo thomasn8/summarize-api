888d86c41767cac197f3c0428a6eea6a6fd5708f
Thomas Nanchen <thomasnanchen@hotmail.com>
2024-05-30 21:31:45 +0200

    pseudo code

--- back/src/summarize/summarize.service.ts
+++ back/src/summarize/summarize.service.ts
@@ -4,6 +4,22 @@ import { RequestDto } from './dto/request.dto';
 @Injectable()
 export class SummarizeService {
   async getSummary(request: RequestDto): Promise<string> {
+    // code here ...
+
+    // parse request.urls
+
+    // scrap web pages content
+    // (Cheerio if only manipulate the dom, Pupeeter can runs javascript to simulate a browser)
+    // get metadatas
+    // if not video, get content
+    // else, get translations
+
+    // get summaries with chatgpt
+
+    // if more than 1 links merge summaries in one summary
+
+    // return summary
+
     return JSON.stringify(request);
   }
 }
